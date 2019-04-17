e802e0f6553296783f9c6b1b77116eece1fe47a3
Rafael Ribeiro <rfribeiro@gmail.com>
2019-04-17 01:38:02 -0300

    web: fix cache import

--- web/project/__init__.py
+++ web/project/__init__.py
@@ -6,14 +6,11 @@ from os.path import join, isfile
 
 from flask import Flask, render_template, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
-#from flask_login import LoginManager
-#from flask_bcrypt import Bcrypt
-#from flask_mail import Mail
-#from flask_uploads import UploadSet, IMAGES, configure_uploads
-#from flask_pagedown import PageDown
+from flask_caching import Cache
+import pylibmc
+from flask_session import Session
 from flask_migrate import Migrate
-#from flask_httpauth import HTTPBasicAuth
-
+import os
 
 def create_cache(app):
     print('Creating cache!')
