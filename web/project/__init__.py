55b31d0a5a7031da726093829bbf06921320aa1b
Rafael Ribeiro <rfribeiro@gmail.com>
2019-04-23 00:31:41 -0300

    fix cache simple

--- web/project/__init__.py
+++ web/project/__init__.py
@@ -12,50 +12,6 @@ from flask_session import Session
 from flask_migrate import Migrate
 import os
 
-def create_cache(app):
-    print('Creating cache!')
-    app.cache = Cache()
-
-    cache_servers = app.config['CACHE_MEMCACHED_SERVERS']
-    if cache_servers == None:
-        app.cache.init_app(app, config={'CACHE_TYPE': 'simple'})
-        print('simple cache')
-    else:
-        cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
-        cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
-        app.cache.init_app(app,
-            config={'CACHE_TYPE': 'memcached',
-                    'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
-                    'CACHE_MEMCACHED_USERNAME': app.config['CACHE_MEMCACHED_USERNAME'],
-                    'CACHE_MEMCACHED_PASSWORD': app.config['CACHE_MEMCACHED_PASSWORD']
-                    })
-        print('memcached cache')
-        app.config.update(
-            SESSION_TYPE = 'memcached',
-            SESSION_MEMCACHED =
-                pylibmc.Client(cache_servers.split(','), binary=True,
-                               username=cache_user, password=cache_pass,
-                               behaviors={
-                                    # Faster IO
-                                    'tcp_nodelay': True,
-                                    # Keep connection alive
-                                    'tcp_keepalive': True,
-                                    # Timeout for set/get requests
-                                    'connect_timeout': 2000, # ms
-                                    'send_timeout': 750 * 1000, # us
-                                    'receive_timeout': 750 * 1000, # us
-                                    '_poll_timeout': 2000, # ms
-                                    # Better failover
-                                    'ketama': True,
-                                    'remove_failed': 1,
-                                    'retry_timeout': 2,
-                                    'dead_timeout': 10,
-                               })
-        )
-
-    Session(app)
-    
-    return app
 
 ################
 #### config ####
@@ -67,13 +23,58 @@ if isfile(join('instance', 'flask_full.cfg')):
 else:
     app.config.from_pyfile('flask.cfg')
 
+app.secret_key = os.urandom(24)
+
+app.config['SESSION_TYPE'] = 'filesystem'
+
 print('Creating db!')
 db = SQLAlchemy(app)
 
 print('migrating db')
 migrate = Migrate(app, db)
 
-app = create_cache(app)
+print('Creating cache!')
+app.cache = Cache()
+
+#cache_servers = app.config['CACHE_MEMCACHED_SERVERS']
+print('cache servers: ', cache_servers)
+#if cache_servers == None:
+app.cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+print('simple cache')
+#else:
+#    cache_user = os.environ.get('CACHE_MEMCACHED_USERNAME') or ''
+#    cache_pass = os.environ.get('CACHE_MEMCACHED_PASSWORD') or ''
+#    cache.init_app(app,
+#        config={'CACHE_TYPE': 'memcached',
+#                'CACHE_MEMCACHED_SERVERS': ['172.19.0.4:11211'],
+#                'CACHE_MEMCACHED_USERNAME': app.config['CACHE_MEMCACHED_USERNAME'],
+#                'CACHE_MEMCACHED_PASSWORD': app.config['CACHE_MEMCACHED_PASSWORD']
+#                })
+#    print('memcached cache')
+#    app.config.update(
+#        SESSION_TYPE = 'memcached',
+#        SESSION_MEMCACHED =
+#            pylibmc.Client(['172.19.0.4:11211'], binary=True,
+#                           username=cache_user, password=cache_pass,
+#                           behaviors={
+#                                # Faster IO
+#                                'tcp_nodelay': True,
+#                                # Keep connection alive
+#                                'tcp_keepalive': True,
+#                                # Timeout for set/get requests
+#                                'connect_timeout': 2000, # ms
+#                                'send_timeout': 750 * 1000, # us
+#                                'receive_timeout': 750 * 1000, # us
+#                                '_poll_timeout': 2000, # ms
+#                                # Better failover
+#                                'ketama': True,
+#                                'remove_failed': 1,
+#                                'retry_timeout': 2,
+#                                'dead_timeout': 10,
+#                           })
+#    )
+#
+Session(app)
 
 from project.models import Translation
 
